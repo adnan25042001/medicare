b02969887b99ee02eb60ce75556a470612061c4f
Adnan <adnan.hussain.136660@gmail.com>
2023-07-30 18:06:08 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Layout from "./layout/Layout";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <BrowserRouter>
+            <Layout />
+        </BrowserRouter>
+    );
 };
 
 export default App;
